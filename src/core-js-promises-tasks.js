b135c94273b19fc05f959019a148dadf6fcecb7b
the-dmitry <dzmitrygaponenko@gmail.com>
2024-04-01 02:28:20 +0300

    feat: queuPromises done

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -136,8 +136,12 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  return promises.reduce((acc, v) => {
+    return acc.then((res) => {
+      return v.then((val) => `${res}${val}`);
+    });
+  }, Promise.resolve(''));
 }
 
 module.exports = {
